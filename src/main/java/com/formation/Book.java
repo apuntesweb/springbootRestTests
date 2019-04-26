408188c2bf41e35f4ab86b35f9155f766a085e22
Roque rbugadev@gmail.com <rbugadev@gmail.com>
2019-04-26 16:44:00 +0200

    Spring Rest

---
+++ src/main/java/com/formation/Book.java
@@ -0,0 +1,45 @@
+package com.formation;
+
+import java.time.LocalDate;
+
+public class Book {
+
+    private String name;
+    private String author;
+    private LocalDate publicationDate;
+
+    public Book() {
+    }
+
+    public Book(String name, String author, LocalDate publicationDate) {
+        super();
+        this.name = name;
+        this.author = author;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+}
